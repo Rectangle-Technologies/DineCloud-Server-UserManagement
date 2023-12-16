6a8db9e0fa42ddaef626ac88c354d86c9c75e329
Naman Khater <naman22khater@gmail.com>
2023-12-16 12:20:43 +0530

    Base setup com[pleted

---
+++ routers.js
@@ -0,0 +1,8 @@
+const routers = [
+    {
+        path: '/api/healthCheck',
+        router: require('./routes/healthCheck')
+    }
+];
+
+module.exports = routers;
---
+++ utils/emailValidator.js
@@ -0,0 +1,7 @@
+// function to validate email address
+const emailValidator = (email) => {
+    const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailRegex.test(email);
+}
+
+module.exports = emailValidator;
